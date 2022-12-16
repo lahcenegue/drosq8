3e3042afef6919027d9ac51ba674b7e96b91daaf
lahcenegue <lahcene90.lg@gmail.com>
2022-12-16 23:56:36 +0100

    categorie null

--- lib/Views/categories_list.dart
+++ lib/Views/categories_list.dart
@@ -22,54 +22,61 @@ class _CategoriesListState extends State<CategoriesList> {
     hvm.addListener(() {
       setState(() {});
     });
-    return Container(
-      height: MediaQuery.of(context).size.height,
-      padding: const EdgeInsets.all(2),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(6),
-      ),
-      child: hvm.listCateg == null || hvm.listCateg!.isEmpty
-          ? const Center(
-              child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              physics: const ScrollPhysics(),
-              itemCount: hvm.listCateg!.length,
-              itemBuilder: (buildContext, index) {
-                return Container(
-                  margin: const EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(width: 1, color: Colors.grey),
-                    borderRadius: BorderRadius.circular(12),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.7),
-                        offset: const Offset(3, 4),
-                        blurRadius: 5,
+
+    if (hvm.listCateg == null || hvm.listCateg!.isEmpty) {
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    } else {
+      return Container(
+        height: MediaQuery.of(context).size.height,
+        padding: const EdgeInsets.all(2),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(6),
+        ),
+        child: ListView.builder(
+          physics: const ScrollPhysics(),
+          itemCount: hvm.listCateg!.length,
+          itemBuilder: (buildContext, index) {
+            return Container(
+              margin: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(width: 1, color: Colors.grey),
+                borderRadius: BorderRadius.circular(12),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.7),
+                    offset: const Offset(3, 4),
+                    blurRadius: 5,
+                  ),
+                ],
+              ),
+              child: ListTile(
+                title: Text(
+                  hvm.listCateg![index].name,
+                  style: const TextStyle(fontSize: 20),
+                ),
+                trailing: const Icon(Icons.arrow_back_ios_new),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SubCategories(
+                        catId: hvm.listCateg![index].id,
+                        name: hvm.listCateg![index].name,
+                        type: hvm.listCateg![index].type,
                       ),
-                    ],
-                  ),
-                  child: ListTile(
-                    title: Text(
-                      hvm.listCateg![index].name,
-                      style: const TextStyle(fontSize: 20),
                     ),
-                    trailing: const Icon(Icons.arrow_back_ios_new),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => SubCategories(
-                                  catId: hvm.listCateg![index].id,
-                                  name: hvm.listCateg![index].name,
-                                  type: hvm.listCateg![index].type,
-                                )),
-                      );
-                    },
-                  ),
-                );
-              }),
-    );
+                  );
+                },
+              ),
+            );
+          },
+        ),
+      );
+    }
   }
 }
--- lib/Views/subcategories.dart
+++ lib/Views/subcategories.dart
@@ -34,66 +34,67 @@ class _SubCategoriesState extends State<SubCategories> {
       setState(() {});
     });
 
-    return Directionality(
-      textDirection: TextDirection.rtl,
-      child: hvm.listSubCateg == null || hvm.listMatter == null
-          ? const Scaffold(
-              body: Center(
-                child: CircularProgressIndicator(),
-              ),
-            )
-          : DefaultTabController(
-              length:
-                  hvm.listSubCateg!.isEmpty || hvm.listMatter!.isEmpty ? 1 : 2,
-              child: Scaffold(
-                appBar: AppBar(
-                  title: Text(widget.name),
-                  bottom: hvm.listSubCateg!.isEmpty
+    if (hvm.listSubCateg == null || hvm.listMatter == null) {
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    } else {
+      return Directionality(
+        textDirection: TextDirection.rtl,
+        child: DefaultTabController(
+          length: hvm.listSubCateg!.isEmpty || hvm.listMatter!.isEmpty ? 1 : 2,
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text(widget.name),
+              bottom: hvm.listSubCateg!.isEmpty
+                  ? const TabBar(tabs: [
+                      Tab(text: 'المواد'),
+                    ])
+                  : hvm.listMatter!.isEmpty
                       ? const TabBar(tabs: [
-                          Tab(text: 'المواد'),
+                          Tab(text: 'الأقسام'),
                         ])
-                      : hvm.listMatter!.isEmpty
-                          ? const TabBar(tabs: [
-                              Tab(text: 'الأقسام'),
-                            ])
-                          : const TabBar(tabs: [
-                              Tab(text: 'الأقسام'),
-                              Tab(text: 'المواد'),
-                            ]),
-                ),
-                body: hvm.listSubCateg!.isEmpty
+                      : const TabBar(tabs: [
+                          Tab(text: 'الأقسام'),
+                          Tab(text: 'المواد'),
+                        ]),
+            ),
+            body: hvm.listSubCateg!.isEmpty
+                ? TabBarView(children: [
+                    // المواد
+                    MatterView(
+                      catId: widget.catId,
+                      type: widget.type,
+                    ),
+                  ])
+                : hvm.listMatter!.isEmpty
                     ? TabBarView(children: [
-                        // المواد
-                        MatterView(
+                        //الاقسام
+                        CategoriesView(
                           catId: widget.catId,
                           type: widget.type,
                         ),
                       ])
-                    : hvm.listMatter!.isEmpty
-                        ? TabBarView(children: [
-                            //الاقسام
-                            CategoriesView(
-                              catId: widget.catId,
-                              type: widget.type,
-                            ),
-                          ])
-                        : TabBarView(
-                            children: [
-                              //الاقسام
-                              CategoriesView(
-                                catId: widget.catId,
-                                type: widget.type,
-                              ),
-
-                              // المواد
-                              MatterView(
-                                catId: widget.catId,
-                                type: widget.type,
-                              ),
-                            ],
+                    : TabBarView(
+                        children: [
+                          //الاقسام
+                          CategoriesView(
+                            catId: widget.catId,
+                            type: widget.type,
                           ),
-              ),
-            ),
-    );
+
+                          // المواد
+                          MatterView(
+                            catId: widget.catId,
+                            type: widget.type,
+                          ),
+                        ],
+                      ),
+          ),
+        ),
+      );
+    }
   }
 }
